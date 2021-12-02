a601f75ab7e5f90736030c078c988eb36aa5ddf1
Adkham <83287524+kodirovoff@users.noreply.github.com>
2021-12-02 20:37:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 'use strick'
 
+const keyPress = document.addEventListener('keypress',function(event){
+    if (event.key === "w"){
+        const music = new Audio('sounds/crash.mp3');
+        music.play();
+    }else if (event.key === "a"){
+        const music = new Audio('sounds/kick-bass.mp3');
+        music.play();
+    }else if (event.key === "s"){
+        const music = new Audio('sounds/snare.mp3');
+        music.play();
+    }else if (event.key === "j"){
+        const music = new Audio('sounds/tom-1.mp3');
+        music.play();
+    }else if (event.key === "d"){
+        const music = new Audio('sounds/tom-2.mp3');
+        music.play();
+    }else if (event.key === "l"){
+        const music = new Audio('sounds/tom-3.mp3');
+        music.play();
+    }else if (event.key === "k"){
+        const music = new Audio('sounds/tom-4.mp3');
+        music.play();
+    }
+});
 for(let i = 0; i<document.querySelectorAll('button').length; i++){
 
 document.querySelectorAll('.drum')[i].addEventListener('click', () =>{
-    if (i === 0){
+    if (i === 0 ||keyPress === 2){
         const music = new Audio('sounds/crash.mp3');
         music.play();
         // document.getElementById('w').style.width = '200px'
